21f7be7cccccda884975f0e228e662a90a151a64
Mikhail Lukyanchikov <gipermike@mail.ru>
2022-02-11 17:39:56 +0300

    hw1

--- Netology_IB_Instruments'/Netology_IB_Instruments'/ProfileView.swift
+++ Netology_IB_Instruments'/Netology_IB_Instruments'/ProfileView.swift
@@ -7,14 +7,38 @@
 
 import UIKit
 
-class ProfileView: UIView {
+@IBDesignable class ProfileView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var dateBurnLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var someTextView: UITextView!
+    
+    
+    var view: UIView!
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        xibSetup()
+        
+
     }
-    */
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        xibSetup()
+    }
+    func xibSetup() {
+        view = loadViewFromNib()
+        view.frame = bounds
+        view.autoresizingMask = [UIView.AutoresizingMask.flexibleWidth, UIView.AutoresizingMask.flexibleHeight]
+        addSubview(view)
+    }
+    func loadViewFromNib() -> UIView {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: "ProfileView", bundle: bundle)
+        let view = nib.instantiate(withOwner: self, options: nil)[0] as! UIView
+        return view
+    }
 
 }
+
