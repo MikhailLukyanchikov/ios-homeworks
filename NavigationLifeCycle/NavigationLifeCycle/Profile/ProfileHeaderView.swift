01307353773bd6aa9959eb5f8db23103b5d20d9c
Mikhail Lukyanchikov <gipermike@mail.ru>
2022-03-09 18:21:19 +0300

    hw3_add label button textfield

--- NavigationLifeCycle/NavigationLifeCycle/Profile/ProfileHeaderView.swift
+++ NavigationLifeCycle/NavigationLifeCycle/Profile/ProfileHeaderView.swift
@@ -7,6 +7,23 @@
 
 import UIKit
 
-class ProfileHeaderView: UIView {
+class ProfileHeaderView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
