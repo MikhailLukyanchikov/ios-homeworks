add43d34124d88d5fa475906bd6aee4d5487b0f9
Mikhail Lukyanchikov <gipermike@mail.ru>
2022-03-09 18:21:26 +0300

    hw3_add label button textfield

--- NavigationLifeCycle/NavigationLifeCycle/Profile/ProfileHeaderView.swift
+++ NavigationLifeCycle/NavigationLifeCycle/Profile/ProfileHeaderView.swift
@@ -7,23 +7,59 @@
 
 import UIKit
 
-class ProfileHeaderView: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+class ProfileHeaderView: UIView {
     
+    override init(frame: CGRect) {
+           super.init(frame: frame)
+           setupView()
+       }
+       required init?(coder: NSCoder) {
+           super.init(coder: coder)
+       }
+    //let avatar = UITextView(frame: CGRect(x: 200, y: 290, width: 190, height: 50))
 
-    /*
-    // MARK: - Navigation
+       private func setupView() {
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+        let profileImage = UIImage(named: "image")
+        let profileImageView = UIImageView(image: profileImage)
+        profileImageView.frame = CGRect(x: 16, y: 116, width: 150, height: 150)
+        profileImageView.layer.masksToBounds = true
+        profileImageView.layer.cornerRadius = 75
+        self.addSubview(profileImageView)
+        
+        let textField = UITextField(frame: CGRect(x: 175, y: 210, width: 180, height: 30))
+        textField.placeholder = "Waiting for something..."
+        textField.returnKeyType = .done
+        textField.autocapitalizationType = .words
+        textField.font = .systemFont(ofSize: 15)
+        textField.textColor = .darkGray
+        textField.backgroundColor = .lightGray
+        textField.borderStyle = .none
+        textField.returnKeyType = .next
+        textField.keyboardType = .default
+        textField.clearButtonMode = .always
+        self.addSubview(textField)
+        
+        let viewLabel = UILabel(frame: CGRect(x: 175, y: 130, width: 180, height: 40))
+        viewLabel.text = "NATURE"
+        viewLabel.font = .systemFont(ofSize: 20, weight: .light)
+        self.addSubview(viewLabel)
+
+        let profileButton = UIButton(frame: CGRect(x: 16, y: 280, width: 360, height: 50))
+        profileButton.setTitle("Show Status", for: .normal)
+        profileButton.backgroundColor = .systemBlue
+        profileButton.layer.cornerRadius = 18
+        self.addSubview(profileButton)
+        
+        
+//           let profileImage = UIImage(named: "image")
+//           let profileImageView = UIImageView(image: profileImage)
+//           profileImageView.frame = CGRect(x: 35, y: 150, width: 150, height: 150)
+//           self.addSubview(profileImageView)
+        
+//        avatar.backgroundColor = .red
+//        self.addSubview(avatar)
+
+       }
 
 }
