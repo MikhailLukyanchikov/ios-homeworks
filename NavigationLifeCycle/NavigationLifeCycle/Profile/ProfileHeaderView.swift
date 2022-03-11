93b42243315d257f44d2605abb550dc6ab800162
Mikhail Lukyanchikov <gipermike@mail.ru>
2022-03-12 00:14:23 +0300

    hw3_add func buttonPressed

--- NavigationLifeCycle/NavigationLifeCycle/Profile/ProfileHeaderView.swift
+++ NavigationLifeCycle/NavigationLifeCycle/Profile/ProfileHeaderView.swift
@@ -12,13 +12,18 @@ class ProfileHeaderView: UIView {
     override init(frame: CGRect) {
            super.init(frame: frame)
            setupView()
+           let buttonPress = UITapGestureRecognizer(target: self, action: #selector(press(_ :)))
+           self.addGestureRecognizer(buttonPress)
        }
        required init?(coder: NSCoder) {
            super.init(coder: coder)
        }
-    //let avatar = UITextView(frame: CGRect(x: 200, y: 290, width: 190, height: 50))
+    
+    let textField = UITextField(frame: CGRect(x: 175, y: 210, width: 180, height: 30))
+    let avatar = UITextView(frame: CGRect(x: 200, y: 290, width: 190, height: 50))
+    let profileButton = UIButton(frame: CGRect(x: 55, y: 410, width: 300, height: 60))
 
-       private func setupView() {
+        private func setupView() {
 
         let profileImage = UIImage(named: "image")
         let profileImageView = UIImageView(image: profileImage)
@@ -27,7 +32,6 @@ class ProfileHeaderView: UIView {
         profileImageView.layer.cornerRadius = 75
         self.addSubview(profileImageView)
         
-        let textField = UITextField(frame: CGRect(x: 175, y: 210, width: 180, height: 30))
         textField.placeholder = "Waiting for something..."
         textField.returnKeyType = .done
         textField.autocapitalizationType = .words
@@ -46,10 +50,16 @@ class ProfileHeaderView: UIView {
         self.addSubview(viewLabel)
 
         let profileButton = UIButton(frame: CGRect(x: 16, y: 280, width: 360, height: 50))
+        profileButton.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
         profileButton.setTitle("Show Status", for: .normal)
         profileButton.backgroundColor = .systemBlue
         profileButton.layer.cornerRadius = 18
+        profileButton.layer.shadowColor = UIColor.black.cgColor
+        profileButton.layer.shadowOffset = CGSize(width: 4, height: 4)
+        profileButton.layer.shadowRadius = 4
+        profileButton.layer.shadowOpacity = 0.7
         self.addSubview(profileButton)
+            
         
         
 //           let profileImage = UIImage(named: "image")
@@ -61,5 +71,33 @@ class ProfileHeaderView: UIView {
 //        self.addSubview(avatar)
 
        }
+    
+        @objc func buttonPressed() {
+            print ("Button pressed")
+            let txt = textField.text!
+            print(txt)
+        }
+
+//    @objc func buttonPressed() {
+//        print ("Button pressed")
+//        avatar.text = textField.text
+//        avatar.textColor = .black
+//        UIView.animate(withDuration: 2.0) {
+//            self.textField.text = ""
+//            UIView.animate(withDuration: 1.0) {
+//                self.profileButton.frame = CGRect(x: 55, y: 428, width: 300, height: 60)
+//                self.textField.alpha = 1
+//                self.endEditing(true)
+//                if self.avatar.hasText {
+//                    self.profileButton.setTitle("Change Status", for: .normal)
+//                    self.textField.alpha = 0
+//                    self.profileButton.frame = CGRect(x: 55, y: 400, width: 300, height: 60)
+//                }
+//            }
+//        }
+//    }
+    @objc func press(_ sender: Any) {
+            textField.resignFirstResponder()
+        }
 
 }
