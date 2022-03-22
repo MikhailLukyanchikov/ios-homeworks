46854b16d110acd0285230d6482566b5c356211a
Mikhail Lukyanchikov <gipermike@mail.ru>
2022-03-22 16:04:14 +0300

    HW4_add button to feedView

--- NavigationLifeCycle/NavigationLifeCycle/FeedViewController.swift
+++ NavigationLifeCycle/NavigationLifeCycle/FeedViewController.swift
@@ -8,22 +8,52 @@
 import UIKit
 
  class FeedViewController: UIViewController {
+    
+    private lazy var firstButton : UIButton = {
+        let firstButton = UIButton()
+        firstButton.backgroundColor = .gray
+        firstButton.setTitle("First Button", for: .normal)
+        firstButton.backgroundColor = .orange
+        firstButton.translatesAutoresizingMaskIntoConstraints = false
+        firstButton.addTarget(self, action: #selector(self.buttonClicked), for: .touchUpInside)
+        return firstButton
+    }()
+    private lazy var secondButton : UIButton = {
+        let secondButton = UIButton()
+        secondButton.backgroundColor = .orange
+        secondButton.setTitle("Second Button", for: .normal)
+        secondButton.translatesAutoresizingMaskIntoConstraints = false
+        secondButton.addTarget(self, action: #selector(self.buttonClicked), for: .touchUpInside)
+        return secondButton
+    }()
+    
+    private lazy var labelStackView : UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.spacing = 10
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Feed"
         view.backgroundColor = .yellow
-        let button = UIButton(frame: CGRect(x: 200, y: 200, width: 100, height: 30))
-        button.backgroundColor = .blue
-        button.setTitle("Click Me", for: .normal)
-        button.layer.cornerRadius = 15
-        button.addTarget(self, action: #selector(buttonClicked), for: .touchUpInside)
-        self.view.addSubview(button)
+        drawself()
         
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 90).isActive = true
-        button.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -90).isActive = true
-        button.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -90).isActive = true
-        button.heightAnchor.constraint(equalToConstant: 40).isActive = true
+    }
+    @objc func drawself(){
+        self.labelStackView.addArrangedSubview(self.firstButton)
+        self.labelStackView.addArrangedSubview(self.secondButton)
+        self.view.addSubview(labelStackView)
+        
+        let infotopConstraint = self.labelStackView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 150)
+        let infoleadingConstraint = self.labelStackView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20)
+        let infotrailingConstraint = self.labelStackView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -20)
+        let infoheightConstraint = self.labelStackView.heightAnchor.constraint(equalToConstant: 154)
+        
+        NSLayoutConstraint.activate([infotopConstraint, infoheightConstraint,infoleadingConstraint,infotrailingConstraint])
     }
     @objc func buttonClicked() {
         let postViewController = PostViewController()
